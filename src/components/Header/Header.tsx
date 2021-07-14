c3db8e4ad46de34c90bc08da49e7b2bccabe09c3
EllisRoss <noumager@gmail.com>
2021-07-14 23:55:29 +0300

    add search function and header background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'antd/dist/antd.css';
 import {Button, Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import styles from './Header.module.css'
+import {useDispatch} from "react-redux";
+import {getBooks} from "../../redux/searchBooksReducer";
 
 
 export const Header: React.FC = React.memo(() => {
 
+    const [query, setQuery] = useState('');
 
+    const dispatch = useDispatch();
+
+    const onSearch = () => {
+        if (query.length > 0) {
+            dispatch(getBooks(query));
+        }
+    }
 
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.heading}>
-                <b>Search for books</b>
-            </div>
+        <div className={styles.background}>
+            <div className={styles.header}>
+                <div className={styles.heading}>
+                    <b>Search for books</b>
+                </div>
 
-            <div className={styles.input}>
-                <Input />
-            </div>
-            <div className={styles.button}>
-                <Button shape="circle" icon={<SearchOutlined/>}/>
+                <div className={styles.input}>
+                    <Input value={query} onChange={e => (setQuery(e.target.value))}/>
+                </div>
+                <div className={styles.button}>
+                    <Button onClick={onSearch} shape="circle" icon={<SearchOutlined/>}/>
+                </div>
             </div>
         </div>
     );
