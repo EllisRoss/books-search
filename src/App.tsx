95ad21390ddde4466b126d52457fab58633922d5
EllisRoss <noumager@gmail.com>
2021-07-14 23:53:06 +0300

    add type for get books data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import './App.css';
+import styles from './App.module.css';
 import store from './redux/store';
 import 'antd/dist/antd.css';
-import { Header } from './components/Header/Header';
+import {Header} from './components/Header/Header';
 import {Books} from "./components/Books/Books";
 
 const App: React.FC = React.memo(() => {
     return (
-        <div className="App">
-            <Header />
-            <Books />
+        <div>
+            <Header/>
+            <Books/>
         </div>
     );
 })
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
+import {Book} from "../types/types";
 
 const apiKey = 'AIzaSyCu8udk6K9EHsV4AZHSJPxNO7B13Ka77A0';
 
 export const searchBooksAPI = {
     getBooks: (query: string) => {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`)
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&maxResults=10&startIndex=0`)
+            .then(response => response.data);
     }
+}
+
+export type GetBooksResponseData = {
+    items: Book[],
+    kind: string,
+    totalItems: number,
 }
