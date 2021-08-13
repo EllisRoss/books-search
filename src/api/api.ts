1b6f4ee6c44ccd2cd35006e38a6b3ccc33ad79f1
EllisRoss <noumager@gmail.com>
2021-08-13 19:00:37 +0300

    add getBook response

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,25 @@
 import axios from "axios";
 import { Book, Categories, SortingBy } from "../types/types";
 
+const instance = axios.create({
+    baseURL: 'https://www.googleapis.com/books/v1/volumes',
+})
+
 const apiKey = 'AIzaSyCu8udk6K9EHsV4AZHSJPxNO7B13Ka77A0';
 
 export const searchBooksAPI = {
     getBooks: (query: string, categories: Categories, sortingBy: SortingBy, startIndex = 0) => {
         if (categories === "all") {
-            return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}
+            return instance.get(`?q=${query}
             &key=${apiKey}&maxResults=30&startIndex=${startIndex}&orderBy=${sortingBy}`)
                 .then(response => response.data);
         }
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}+subject:${categories}
+        return instance.get(`?q=${query}+subject:${categories}
             &key=${apiKey}&maxResults=30&startIndex=${startIndex}&orderBy=${sortingBy}`)
             .then(response => response.data);
+    },
+    getBook: (id: string) => {
+        return instance.get(`/${id}?key=${apiKey}`).then(response => response.data);
     }
 }
 
