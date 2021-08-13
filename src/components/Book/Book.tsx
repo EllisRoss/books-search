f84723ee54248374cc269bca599f9997bc986d2f
EllisRoss <noumager@gmail.com>
2021-08-13 17:42:20 +0300

    add bookCover func

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -3,18 +3,40 @@ import {useSelector} from "react-redux";
 import {selectBookInfo} from "../../redux/searchBooksSelectors";
 import {Col, Row} from "antd";
 import styles from "./Book.module.css"
+import bookPlaceholder from "../../assets/images/book-placeholder.png";
 
 
 export const Book: React.FC = React.memo(() => {
 
     const bookInfo = useSelector(selectBookInfo);
 
+    const bookCover = (): string | undefined => {
+        if (bookInfo) {
+            if (bookInfo.volumeInfo.imageLinks?.extraLarge) {
+                return bookInfo.volumeInfo.imageLinks?.extraLarge;
+            }
+            if (bookInfo.volumeInfo.imageLinks?.large) {
+                return bookInfo.volumeInfo.imageLinks?.large;
+            }
+            if (bookInfo.volumeInfo.imageLinks?.medium) {
+                return bookInfo.volumeInfo.imageLinks?.medium;
+            }
+            if (bookInfo.volumeInfo.imageLinks?.small) {
+                return bookInfo.volumeInfo.imageLinks?.small;
+            }
+            if (bookInfo.volumeInfo.imageLinks?.thumbnail) {
+                return bookInfo.volumeInfo.imageLinks?.thumbnail;
+            }
+        }
+        return undefined;
+    }
+
     if (bookInfo) {
         return (
             <div className={styles.wrapper}>
                 <Row>
                     <Col className={styles.coverWrapper} span={5}>
-                        <img src={bookInfo.volumeInfo.imageLinks?.thumbnail} alt=""/>
+                        <img src={bookCover() ? bookCover() : bookPlaceholder} alt="book's cover"/>
                     </Col>
                     <Col span={15}>
                         {
