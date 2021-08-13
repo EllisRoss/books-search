9fd6e400fa89351cc681598c1375a44a06e194d5
EllisRoss <noumager@gmail.com>
2021-08-13 17:46:40 +0300

    update css classes in App and add basename in BrowserRouter for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import 'antd/dist/antd.css';
+import styles from "./App.module.css";
 import {Header} from './components/Header/Header';
 import {Books} from "./components/Books/Books";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
@@ -24,21 +25,22 @@ const App: React.FC = React.memo(() => {
     return (
         <div>
             <Header/>
-            <Switch>
-                <Route exact path='/' render={() => <Redirect to='/books'/> } />
-                <Route path='/books' render={() => <Books/> } />
-                <Route path='/book/:id?' render={() => <Book/> } />
-                <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
-            </Switch>
+            <div className={styles.content}>
+                <Switch>
+                    <Route exact path='/' render={() => <Redirect to='/books'/> } />
+                    <Route path='/books' render={() => <Books/> } />
+                    <Route path='/book/:id?' render={() => <Book/> } />
+                    <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+                </Switch>
+            </div>
         </div>
     );
 })
 
-
 export const SearchBooksApp: React.FC = () => {
     return (
         <React.StrictMode>
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Provider store={store}>
                     <App/>
                 </Provider>
