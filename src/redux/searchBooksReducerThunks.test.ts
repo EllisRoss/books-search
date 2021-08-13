a0210d4a04ff464004534e27c41a1abcf770aae6
EllisRoss <noumager@gmail.com>
2021-08-13 19:02:31 +0300

    add test for getBook thunk

--- src/redux/searchBooksReducerThunks.test.ts
+++ src/redux/searchBooksReducerThunks.test.ts
@@ -1,6 +1,6 @@
 import {GetBooksResponseData, searchBooksAPI} from "../api/api";
-import {getBooks, loadMoreBooks, searchBooksActions} from "./searchBooksReducer";
-import {Filter} from "../types/types";
+import {getBook, getBooks, loadMoreBooks, searchBooksActions} from "./searchBooksReducer";
+import {Book, Filter} from "../types/types";
 
 jest.mock("../api/api");
 const searchAPIMock = searchBooksAPI as jest.Mocked<typeof searchBooksAPI>;
@@ -204,7 +204,104 @@ const result: GetBooksResponseData = {
     ],
     totalItems: 0,
     kind: 'kind'
-}
+};
+const bookResponseResult: Book = {
+        kind: "books#volume",
+        id: "MqRXAwAAQBAJ",
+        etag: "xpFiO0GaUL0",
+        selfLink: "https://www.googleapis.com/books/v1/volumes/MqRXAwAAQBAJ",
+        volumeInfo: {
+            pageCount: 322,
+            printedPageCount: 322,
+            title: "Node.js Разработка серверных веб-приложений на JavaScript",
+            authors: [
+                "Дэвид Хэррон"
+            ],
+            publisher: "Litres",
+            publishedDate: "2017-09-05",
+            description: "Книга посвящена разработке веб-приложений в Node.js – платформе, которая выводит язык JavaScript за пределы браузера и позволяет использовать его в серверных приложениях. В основе платформы лежит исключительно быстрый движок JavaScript, заимствованный из браузера Chrome, к которому добавлена быстрая и надежная библиотека асинхронного сетевого ввода/вывода. Основной упор в Node.js делается на создании высокопроизводительных, хорошо масштабируемых клиентских и серверных приложений.На практических примерах вы научитесь пользоваться серверным и клиентским объектами HTTP, каркасами Connect и Express, освоите алгоритмы асинхронного выполнения и узнаете, как работать с базами данных на основе SQL и с MongoDB.Начав с практических рекомендаций по установке и настройке Node.js в режиме разработки и эксплуатации, вы научитесь разрабатывать клиентские и серверные HTTP-приложения; познакомитесь с применяемой в Node.js системой организации модулей на основе спецификации CommonJS, позволяющей реализовать подмножество технологии объектно-ориентированного проектирования.Издание предназначено для программистов, знакомых с основами JavaScript и веб-разработки.",
+            industryIdentifiers: [
+                {
+                    type: "ISBN_13",
+                    identifier: "9785457573277"
+                },
+                {
+                    type: "ISBN_10",
+                    identifier: "5457573273"
+                }
+            ],
+            readingModes: {
+                text: false,
+                image: true
+            },
+            printType: "BOOK",
+            categories: [
+                "Computers"
+            ],
+            maturityRating: "NOT_MATURE",
+            allowAnonLogging: true,
+            contentVersion: "1.0.0.0.preview.1",
+            panelizationSummary: {
+                containsEpubBubbles: false,
+                containsImageBubbles: false
+            },
+            imageLinks: {
+                smallThumbnail: "http://books.google.com/books/content?id=MqRXAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                thumbnail: "http://books.google.com/books/content?id=MqRXAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            language: "un",
+            previewLink: "http://books.google.ru/books?id=MqRXAwAAQBAJ&printsec=frontcover&dq=js&hl=&cd=1&source=gbs_api",
+            infoLink: "https://play.google.com/store/books/details?id=MqRXAwAAQBAJ&source=gbs_api",
+            canonicalVolumeLink: "https://play.google.com/store/books/details?id=MqRXAwAAQBAJ"
+        },
+        saleInfo: {
+            country: "RU",
+            saleability: "FOR_SALE",
+            isEbook: true,
+            listPrice: {
+                amount: 240.12,
+                currencyCode: "RUB"
+            },
+            retailPrice: {
+                amount: 216.11,
+                currencyCode: "RUB"
+            },
+            buyLink: "https://play.google.com/store/books/details?id=MqRXAwAAQBAJ&rdid=book-MqRXAwAAQBAJ&rdot=1&source=gbs_api",
+            offers: [
+                {
+                    finskyOfferType: 1,
+                    listPrice: {
+                        amountInMicros: 240120000,
+                        currencyCode: "RUB"
+                    },
+                    retailPrice: {
+                        amountInMicros: 216110000,
+                        currencyCode: "RUB"
+                    }
+                }
+            ]
+        },
+        accessInfo: {
+            country: "RU",
+            viewability: "PARTIAL",
+            embeddable: true,
+            publicDomain: false,
+            textToSpeechPermission: "ALLOWED",
+            epub: {
+                isAvailable: false
+            },
+            pdf: {
+                isAvailable: true,
+                acsTokenLink: "http://books.google.ru/books/download/Node_js_%D0%A0%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0_%D1%81%D0%B5%D1%80%D0%B2%D0%B5-sample-pdf.acsm?id=MqRXAwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+            },
+            webReaderLink: "http://play.google.com/books/reader?id=MqRXAwAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+            accessViewStatus: "SAMPLE",
+            quoteSharingAllowed: false
+        },
+        searchInfo: {
+            textSnippet: "Книга посвящена разработке веб-приложений в Node.js – платформе, которая выводит язык JavaScript за пределы браузера и позволяет ..."
+        }
+    };
 
 const dispatchMock = jest.fn();
 const getStateMock = jest.fn();
@@ -266,4 +363,17 @@ test("load more books thunk success", async () => {
 
     expect(dispatchMock).toHaveBeenNthCalledWith(4,
         searchBooksActions.isFetchingMoreBooksChanged(false))
+})
+
+test("get book thunk success", async () => {
+
+    searchAPIMock.getBook.mockResolvedValue(bookResponseResult);
+    const thunk = getBook('MqRXAwAAQBAJ');
+
+    await thunk(dispatchMock, getStateMock, {});
+
+    expect(dispatchMock).toBeCalledTimes(1)
+
+    expect(dispatchMock).toHaveBeenNthCalledWith(1,
+        searchBooksActions.bookInfoChanged(bookResponseResult))
 })
